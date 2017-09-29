831254ee961a63a22781e4f591ac7aae1770fdc5
AndjDavis <andi.idavis@gmail.com>
2017-09-28 21:39:59 -0600

    client side with folders and file structures completed not including redux, no route set up

---
+++ client/src/main/routes/gameroom/gameRoomContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import GameRoomComponent from "./gameRoomComponent"
+
+class GameRoomContainer extends Component {
+  render() {
+    return (
+      <div>
+        <GameRoomComponent />
+      </div>
+    );
+  }
+}
+
+export default GameRoomContainer;
---
+++ client/src/main/routes/p-y-l/pylContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PylComponent from "./pylComponent"
+
+class PylContainer extends Component {
+  render() {
+    return (
+      <div>
+        <PylComponent />
+      </div>
+    );
+  }
+}
+
+export default PylContainer;
