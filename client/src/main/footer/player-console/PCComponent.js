6022c20b2626105362588679f4d1663dbcd68c06
AndjDavis <andi.idavis@gmail.com>
2017-09-28 21:28:08 -0600

    Client side with folders and file structures completed

---
+++ client/src/main/footer/player-console/PCComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PCComponent extends Component {
+  render() {
+    return (
+      <div>
+        PC Component
+      </div>
+    );
+  }
+}
+
+export default PCComponent;
