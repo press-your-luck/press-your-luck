e92539724fe3480b050895a3e05dc0d709315195
Curtis Young <curtiscyoung@gmail.com>
2017-10-03 17:44:00 -0600

    added login functionality to frontend

--- client/src/main/routes/login/LoginComponent.js
+++ client/src/main/routes/login/LoginComponent.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-class SignUpComponent extends Component {
-  render() {
-    return (
-       <div>
-            <span>Sign Up!</span>
+class LoginComponent extends Component {
+    render() {
+        return (
+        <div>
+            <span>Login</span>
                 <form onSubmit={this.props.handleSubmit}>
                     <input onChange={this.props.handleChange}
                         value={this.props.inputs.username}
@@ -16,16 +16,11 @@ class SignUpComponent extends Component {
                         name="password"
                         type="password"
                         placeholder="password" />
-                    <input onChange={this.props.handleChange}
-                        value={this.props.inputs.email}
-                        name="email"
-                        type="text"
-                        placeholder="email" />
                     <button type="submit">CREATE ACCOUNT</button>
                 </form>
             </div>
-    );
-  }
+        );
+    }
 }
 
-export default SignUpComponent;
+export default LoginComponent;
---
+++ client/src/main/routes/login/LoginContainer.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import LoginComponent from "./LoginComponent.js";
+import { login } from "../../redux/actions/action";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+
+class LoginContainer extends Component {
+    constructor() {
+        super()
+        this.state = {
+          inputs: {
+            username: "",
+            password: "",
+            email: ""
+          }
+        }
+      }
+      handleChange = (e) => {
+        e.persist();
+        this.setState((prevState) => {
+          return {
+            inputs: {
+              ...prevState.inputs,
+              [e.target.name]: e.target.value
+            }
+          }
+        })
+      }
+      handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.login(this.state.inputs);
+      }
+    
+    
+    render() {
+        return (
+            this.props.isAuthenticated ?
+            <Redirect to="/gameroom" /> :
+            <LoginComponent 
+            handleSubmit={this.handleSubmit} 
+            handleChange={this.handleChange} 
+            authMsg={this.props.authError.login} 
+            {...this.state} />
+        );
+    }
+}
+
+const mapStateToProps = function(state){
+    return state
+}
+
+export default connect(mapStateToProps, { login })(LoginContainer);
