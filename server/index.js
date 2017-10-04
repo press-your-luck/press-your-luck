6d93f27e081ae198930a8293b773aa35d24b0a09
AndjDavis <andi.idavis@gmail.com>
2017-10-03 18:44:48 -0600

    one more try

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const config = require("./config");
 const gameRoute = require("./routes/gamedata.js");
 const authRoute = require("./routes/auth.js");
+const playerRoute = require("./routes/player");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use("/game", gameRoute);
 app.use("/auth", authRoute);
+app.use("/player", playerRoute)
 
 
 
