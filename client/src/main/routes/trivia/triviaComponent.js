e6d8e99c22f9e6795b255ac86d03de6b83f293fa
Curtis Young <curtiscyoung@gmail.com>
2017-10-05 20:26:25 -0600

    cleaned up some unnecessary functions.  Made the code easier to read.

--- client/src/main/routes/trivia/triviaComponent.js
+++ client/src/main/routes/trivia/triviaComponent.js
@@ -4,11 +4,13 @@ class TriviaComponent extends Component {
   render() {
     return (
       <div>
-         <p> {this.props.currentQuestion.question} </p>
-        <button onClick={this.props.handleAnswer} name="1">{this.props.currentQuestion.option1}</button>
-        <button onClick={this.props.handleAnswer} name="2">{this.props.currentQuestion.option2}</button>
-        <button onClick={this.props.handleAnswer} name="3">{this.props.currentQuestion.option3}</button>
-        <button onClick={this.props.handleAnswer} name="4">{this.props.currentQuestion.option4}</button>
+        <p className="question"> {this.props.currentQuestion.question} </p>
+        <div className="options">
+          <button onClick={this.props.handleAnswer} name="1">{this.props.currentQuestion.option1}</button>
+          <button onClick={this.props.handleAnswer} name="2">{this.props.currentQuestion.option2}</button>
+          <button onClick={this.props.handleAnswer} name="3">{this.props.currentQuestion.option3}</button>
+          <button onClick={this.props.handleAnswer} name="4">{this.props.currentQuestion.option4}</button>
+        </div>
       </div>
     );
   }
